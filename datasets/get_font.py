0981c9f45b706d0d1bfbb840278b6b4de2a5b5c1
1999ADEK <b06201018@ntu.edu.tw>
2020-02-20 09:26:24 +0800

    Make images with more than one font and allow picking size of images

--- datasets/get_font.py
+++ datasets/get_font.py
@@ -15,25 +15,29 @@ def create_char_img(ch, font, size, x_offset=0, y_offset=0):
     draw.text((x_offset, y_offset), ch, 0, font=font)
     return img
 
-def gen_dataset(filename, store_dir, size=128):
-    font = ImageFont.truetype(filename, size)
+def gen_dataset(style, store_dir, size=128):
     char_list = get_char_list()
     os.mkdir(store_dir)
     os.chdir(store_dir)
-    print('Generating font dataset...')
-    for i, ch in enumerate(char_list):
-        int_unicode = int(ch, 16)
-        img = create_char_img(chr(int_unicode), font, size)
-        img.save(f'font{int_unicode}.bmp')
+    print(f'Generating {style} fonts dataset...')
+    font_dir = f'../{style}-fonts/'
+    for filename in os.listdir(font_dir):
+        print(f'Generating images using {filename}...')
+        font = ImageFont.truetype(font_dir+filename, size)
+        for i, ch in enumerate(char_list):
+            int_unicode = int(ch, 16)
+            img = create_char_img(chr(int_unicode), font, size)
+            img.save(f'{filename[:-4]}{int_unicode}.bmp')
     print('Complete.')
     os.chdir('..')
 
     
 parser = argparse.ArgumentParser(description='Construct font datasets')
 parser.add_argument('--store_dir', dest='store_dir', required=True, help='directory to store the images')
-parser.add_argument('--font_filename', dest='filename', required=True, help='filename of the font file')
+parser.add_argument('--font_style', dest='style', required=True, help='to generate handwritten or calligrapht fonts')
+parser.add_argument('--img_size', dest='size', type=int, default=128, help='generated image size')
 
 args = parser.parse_args()
 
 if __name__ == '__main__':
-    gen_dataset(args.filename, args.store_dir)
+    gen_dataset(args.style, args.store_dir, args.size)
