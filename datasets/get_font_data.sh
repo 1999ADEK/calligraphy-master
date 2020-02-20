ac129e40d7bdb6cacded44910a5d61a322f458a2
1999ADEK <b06201018@ntu.edu.tw>
2020-02-20 09:54:25 +0800

    Delete comment

--- datasets/get_font_data.sh
+++ datasets/get_font_data.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# To train with different font, add the font file to datasets/
-# and change the filename below to the filename of the file.
 python get_font.py --store_dir trainA --font_style handwritten --img_size 128
 python get_font.py --store_dir trainB --font_style calligraphy --img_size 128
